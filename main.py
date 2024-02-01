1750d166ee0b5127ca9c6dea864760f6956bb1cb
lean-dro <leandro.saraiva@sptech.school>
2024-01-31 22:50:06 -0300

    Rota inicial definida com a documentação

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from typing import Union
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Response
+from fastapi.responses import HTMLResponse
+from starlette.responses import FileResponse
+import os
 
-from fastapi import FastAPI
+app = FastAPI(openapi_url=None)
+app.mount("/", StaticFiles(directory="public/docs", html=True), "public")
 
-app = FastAPI()
-
-
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"Hello": "World"}
+    return FileResponse("./public/docs/index.html")
 
